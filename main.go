6a3d446ec032f85eb315e3ed3ce65b8794f622b1
Freddy-Gong <21914103@zju.edu.cn>
2022-03-11 13:15:44 +0800

    传文件

--- main.go
+++ main.go
@@ -98,6 +98,33 @@ func QrcodesController(c *gin.Context) {
 	}
 }
 
+func FilesController(c *gin.Context) {
+	file, err := c.FormFile("raw")
+	if err != nil {
+		log.Fatal(err)
+	}
+	exe, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dir := filepath.Dir(exe)
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := uuid.New().String()
+	uploads := filepath.Join(dir, "uploads")
+	err = os.MkdirAll(uploads, os.ModePerm)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fullpath := path.Join("uploads", filename+filepath.Ext(file.Filename))
+	fileErr := c.SaveUploadedFile(file, filepath.Join(dir, fullpath))
+	if fileErr != nil {
+		log.Fatal(fileErr)
+	}
+	c.JSON(http.StatusOK, gin.H{"url": "/" + fullpath})
+}
+
 //把前端打包到go生产的文件上
 //go:embed frontend/dist/*
 var FS embed.FS
@@ -107,6 +134,7 @@ func main() {
 		gin.SetMode(gin.ReleaseMode)
 		router := gin.Default()
 		staticFiles, _ := fs.Sub(FS, "frontend/dist")
+		router.POST("/api/v1/files", FilesController)
 		router.GET("/api/v1/qrcodes", QrcodesController)
 		router.GET("/uploads/:path", UploadsController)
 		router.POST("/api/v1/texts", TextsController)
