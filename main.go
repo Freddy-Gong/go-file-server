edf739955aa9c2516f86fc723e447b61dd09eff3
Freddy-Gong <21914103@zju.edu.cn>
2022-03-13 12:01:22 +0800

    重构main函数

--- main.go
+++ main.go
@@ -9,23 +9,31 @@ import (
 )
 
 func main() {
-	go func() {
-		server.Run()
-	}()
-	//开一个子进程启动chrome
-	chromePath := "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-	cmd := exec.Command(chromePath, "--app=http://localhost:8080/static/index.html")
-	cmd.Start()
-	//<-chSignal//如果channel中没有值的传入，那就会阻塞在这里
-	//cmd.Process.Kill()
-	//select {} //目的是不让mian进行结束，因为如果main进程结束了，里面开的gin进程
-	//也就会结束，所以就是导致无法访问
-	chSignal := make(chan os.Signal, 1)
-	signal.Notify(chSignal, os.Interrupt) //监听Ctrl+C的信号，有信号就传给channel
+	go server.Run()
+
+	cmd := startBrowser()
+	chSignal := listenToInterrupt()
 	select {
 	case <-chSignal:
 		cmd.Process.Kill()
 	}
+	//<-chSignal//如果channel中没有值的传入，那就会阻塞在这里
+	//cmd.Process.Kill()
+	//select {} //目的是不让mian进行结束，因为如果main进程结束了，里面开的gin进程
+	//也就会结束，所以就是导致无法访问
+}
+
+func startBrowser() *exec.Cmd {
+	chromePath := "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+	cmd := exec.Command(chromePath, "--app=http://localhost:8080/static/index.html")
+	cmd.Start()
+	return cmd
+}
+
+func listenToInterrupt() chan os.Signal {
+	chSignal := make(chan os.Signal, 1)
+	signal.Notify(chSignal, os.Interrupt) //监听Ctrl+C的信号，有信号就传给channel
+	return chSignal
 }
 
 //接口是对方法的约束
